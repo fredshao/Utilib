eb5e1822c582029d832e2a8081477e64712008cc
fredshao <fredshao@aliyun.com>
2018-07-19 11:17:18 +0800

    Add Layer Util and UI Util

---
+++ Utilib/LayerUtil.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 和层有关的一些Util方法
+/// </summary>
+public static class LayerUtil
+{
+    /// <summary>
+    /// 开启摄像机某一层的渲染
+    /// </summary>
+    /// <param name="_cam"></param>
+    /// <param name="_layer"></param>
+    public static void SetRenderLayer(Camera _cam, int _layer)
+    {
+        if(_cam == null)
+        {
+            Debug.LogError("ERROR: Camera Can't be NULL");
+            return;
+        }
+
+        _cam.cullingMask |= (1 << _layer);
+    }
+
+    /// <summary>
+    /// 开启摄相机某一层的渲染
+    /// </summary>
+    /// <param name="_cam"></param>
+    /// <param name="_layerName"></param>
+    public static void SetRenderLayer(Camera _cam, string _layerName)
+    {
+        SetRenderLayer(_cam,LayerMask.NameToLayer(_layerName));
+    }
+
+    /// <summary>
+    /// 关闭摄相机某一层的渲染
+    /// </summary>
+    /// <param name="_cam"></param>
+    /// <param name="_layer"></param>
+    public static void SetUnRenderLayer(Camera _cam, int _layer)
+    {
+        if(_cam == null)
+        {
+            Debug.LogError("ERROR: Camera Can't be NULL");
+            return;
+        }
+
+        _cam.cullingMask &= ~(1 << _layer);
+    }
+
+    /// <summary>
+    /// 关闭摄像机某一层的渲染
+    /// </summary>
+    /// <param name="_cam"></param>
+    /// <param name="_layerName"></param>
+    public static void SetUnRenderLayer(Camera _cam, string _layerName)
+    {
+        SetUnRenderLayer(_cam, LayerMask.NameToLayer(_layerName));
+    }
+
+    /// <summary>
+    /// 某一层是否被渲染
+    /// </summary>
+    /// <param name="_cam"></param>
+    /// <param name="_layer"></param>
+    /// <returns></returns>
+    public static bool IsRenderLayer(Camera _cam, int _layer)
+    {
+        if(_cam == null)
+        {
+            Debug.LogError("ERROR: Camera Can't be NULL");
+            return false;
+        }
+
+        return (_cam.cullingMask & (1 << _layer)) != 0;
+    }
+
+    /// <summary>
+    /// 某一层是否被渲染
+    /// </summary>
+    /// <param name="_cam"></param>
+    /// <param name="_layerName"></param>
+    /// <returns></returns>
+    public static bool IsRenderLayer(Camera _cam, string _layerName)
+    {
+        return IsRenderLayer(_cam, LayerMask.NameToLayer(_layerName));
+    }
+
+    /// <summary>
+    /// 设置一个GameObject为某一层，包括子物体，不管是 Active true 还是 Active false 的子物体
+    /// </summary>
+    /// <param name="_obj"></param>
+    /// <param name="_layer"></param>
+    public static void SetGameObjectToLayer(GameObject _obj, int _layer)
+    {
+        if(_obj == null)
+        {
+            Debug.LogError("ERROR: GameObject Can't be NULL");
+            return;
+        }
+
+        _obj.layer = _layer;
+        foreach(Transform child in _obj.transform)
+        {
+            SetGameObjectToLayer(child.gameObject, _layer);
+        }
+    }
+
+
+    /// <summary>
+    /// 设置一个GameObject为某一层，包括子物体，不管是 Active true 还是 Active false 的子物体
+    /// </summary>
+    /// <param name="_obj"></param>
+    /// <param name="_layer"></param>
+    public static void SetGameObjectToLayer(GameObject _obj, string _layerName)
+    {
+        SetGameObjectToLayer(_obj, LayerMask.NameToLayer(_layerName));
+    }
+
+
+}
---
+++ Utilib/UIUtil.cs
@@ -0,0 +1,188 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 和UI有关的一些Util方法
+/// </summary>
+public static class UIUtil
+{
+    /// <summary>
+    /// 锚点左边
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void AnchorsLeft(RectTransform _rectTrans)
+    {
+        _rectTrans.anchorMin = new Vector2(0, 0.5f);
+        _rectTrans.anchorMax = new Vector2(0, 0.5f);
+    }
+
+    /// <summary>
+    /// 锚点右边
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void AnchorsRight(RectTransform _rectTrans)
+    {
+        _rectTrans.anchorMin = new Vector2(1, 0.5f);
+        _rectTrans.anchorMax = new Vector2(1, 0.5f);
+    }
+
+    /// <summary>
+    /// 锚点上面
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void AnchorsTop(RectTransform _rectTrans)
+    {
+        _rectTrans.anchorMin = new Vector2(0.5f,1);
+        _rectTrans.anchorMax = new Vector2(0.5f,1);
+    }
+
+    /// <summary>
+    /// 锚点下面
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void AnchorsBottom(RectTransform _rectTrans)
+    {
+        _rectTrans.anchorMin = new Vector2(0.5f, 0);
+        _rectTrans.anchorMax = new Vector2(0.5f, 0);
+    }
+
+    /// <summary>
+    /// 锚点左上
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void AnchorsLeftTop(RectTransform _rectTrans)
+    {
+        _rectTrans.anchorMin = new Vector2(0, 1);
+        _rectTrans.anchorMax = new Vector2(0, 1);
+    }
+
+    /// <summary>
+    /// 锚点左下
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void AnchorsLeftBottom(RectTransform _rectTrans)
+    {
+        _rectTrans.anchorMin = new Vector2(0, 0);
+        _rectTrans.anchorMax = new Vector2(0, 0);
+    }
+
+    /// <summary>
+    /// 锚点右上
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void AnchorsRightTop(RectTransform _rectTrans)
+    {
+        _rectTrans.anchorMin = new Vector2(1, 1);
+        _rectTrans.anchorMax = new Vector2(1, 1);
+    }
+
+    /// <summary>
+    /// 锚点右下
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void AnchorsRightBottom(RectTransform _rectTrans)
+    {
+        _rectTrans.anchorMin = new Vector2(1, 0);
+        _rectTrans.anchorMax = new Vector2(1, 0);
+    }
+
+    /// <summary>
+    /// 锚点中心
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void AnchorsCenter(RectTransform _rectTrans)
+    {
+        _rectTrans.anchorMin = new Vector2(0.5f, 0.5f);
+        _rectTrans.anchorMax = new Vector2(0.5f, 0.5f);
+    }
+
+
+    /// <summary>
+    /// 向左拉伸
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void StretchLeft(RectTransform _rectTrans)
+    {
+        _rectTrans.pivot = new Vector2(1, 0.5f);
+    }
+
+    /// <summary>
+    /// 向右拉伸
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void StretchRight(RectTransform _rectTrans)
+    {
+        _rectTrans.pivot = new Vector2(0, 0.5f);
+    }
+
+    /// <summary>
+    /// 向上拉伸
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void StretchUp(RectTransform _rectTrans)
+    {
+        _rectTrans.pivot = new Vector2(0.5f,0);
+    }
+
+    /// <summary>
+    /// 向下拉伸
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void StretchDown(RectTransform _rectTrans)
+    {
+        _rectTrans.pivot = new Vector2(1, 0.5f);
+    }
+
+    /// <summary>
+    /// 向左下拉伸
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void StretchLeftDown(RectTransform _rectTrans)
+    {
+        _rectTrans.pivot = new Vector2(1,1);
+    }
+
+    /// <summary>
+    /// 向左上拉伸
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void StretchLeftUp(RectTransform _rectTrans)
+    {
+        _rectTrans.pivot = new Vector2(1, 0);
+    }
+
+    /// <summary>
+    /// 向右上拉但
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void StretchRightUp(RectTransform _rectTrans)
+    {
+        _rectTrans.pivot = new Vector2(0,0);
+    }
+
+    /// <summary>
+    /// 向右下拉伸
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void StretchRightDown(RectTransform _rectTrans)
+    {
+        _rectTrans.pivot = new Vector2(0,1);
+    }
+
+    /// <summary>
+    /// 左右拉伸
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void StretchLeftRight(RectTransform _rectTrans)
+    {
+        _rectTrans.pivot = new Vector2(0.5f, 0.5f);
+    }
+
+    /// <summary>
+    /// 上下拉伸
+    /// </summary>
+    /// <param name="_rectTrans"></param>
+    public static void StretchUpDown(RectTransform _rectTrans)
+    {
+        _rectTrans.pivot = new Vector2(0.5f, 0.5f);
+    }
+}
